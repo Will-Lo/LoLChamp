4e13406424658d4542ae01244e7b41551a0ebe0e
ian952 <ianhu952@hotmail.com>
2016-05-03 00:12:04 -0400

    Added API call to get Player ID

--- app/server.js
+++ app/server.js
@@ -25,4 +25,50 @@ new WebpackDevServer(webpack(config), {
 	}
 
 	console.log('Development server running at localhost:3001');
+
+
+	getPlayerId ('Ian952','a');
+
 });
+
+var https = require('https');
+var apikey = 'c49048cc-8980-4f7b-a3d1-9c0b551a80ad';
+
+var masteryApi = {
+  host: 'na.api.pvp.net',
+  path: 'a'
+};
+
+function getRequest (request,callback) {
+	https.request (request,function(response){
+		var str = '';
+
+    response.on('data',function(chunk){
+      str += chunk;
+    });
+
+    response.on('end',function(){
+      console.log(str);
+			callback(str);
+    });
+	}).end();
+}
+
+function getPlayerId (name,callback) {
+	var playerApi = {
+		host: 'na.api.pvp.net',
+		path: '/api/lol/na/v1.4/summoner/by-name/'+name+'?api_key='+apikey
+	}
+
+	getRequest(playerApi,function(res) {
+		var player = JSON.parse(res);
+
+		console.log('Player ID: '+player[name.toLowerCase()].id);
+
+		callback(player[name.toLowerCase()].id);
+	});
+}
+
+function getMastery (player, callback) {
+  //get mastery now
+}
